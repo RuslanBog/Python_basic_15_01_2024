3fb1b88a9fcb866c65c7577a3c17cda953166e3f
Ruslan <ruslanboginskiy@gmail.com>
2024-01-22 19:11:42 +0200

    HW_2

--- lesson_2_18_01.py
+++ lesson_2_18_01.py
@@ -1,8 +1,12 @@
 
-numbers = input('Введіть чотирьохзначне число: ')
+numbers = int(input('Введіть 4-х значне число: '))
 
+e = numbers % 10
+d = numbers //10 % 10
+s = numbers // 100 % 10
+r = numbers // 1000
 
-print(numbers[0])
-print(numbers[1])
-print(numbers[2])
-print(numbers[3])
+print(r)
+print(s)
+print(d)
+print(e)
