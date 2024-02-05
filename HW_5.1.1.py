6449836840e3bc23b4686d0642bc19eaa3633d08
Ruslan <ruslanboginskiy@gmail.com>
2024-02-05 17:10:35 +0200

    home_work_5.1

--- HW_5.1.1.py
+++ HW_5.1.1.py
@@ -1,18 +1,25 @@
 import string
-some_str = "6llK"
-name_val = ""
+some_str = "55"
+new_name_val = ""
 
+for name in some_str:
+    if name.isnumeric():
+        new_name_val = False
+    elif name.isupper():
+        new_name_val = False
+    else:
+        new_name_val = True
+print(new_name_val)
+    # if some_str.isnumeric():
+    #     name_val = False
+    # else:
+    #     name_val = True
 # for name in some_str:
 #     if some_str.isnumeric():
-#         name_val = False
-#     else:
-#         name_val = True
-for name in some_str:
-    if some_str.isnumeric():
-        print(False)
-        # break
-    elif some_str.isupper():
-        print(False)
+#         print(False)
+#         # break
+#     elif some_str.isupper():
+#         print(False)
         # break
     # else:
     #     print(True)
@@ -28,10 +35,3 @@ for name in some_str:
 #     name_val = True
 # print(name_val)
 
-    # if name.isnumeric():
-    #     new_name_val = False
-    # if name.isupper():
-    #     new_name_val = False
-    # else:
-    #     new_name_val = True
-# print(new_name_val)
--- HW_5.1.py
+++ HW_5.1.py
@@ -1,25 +1,25 @@
+
+import string
 import keyword
+# дано:
+some_val = "orjb2"
 
-value_int = "23lL"
-new_value_int = ""
-num = '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-while True:
-    print(not value_int.isupper())
+# условие:
+# 1. не може починатися з цифри - startswith() - False
+# 2. не може складатися тільки з цифр - .isnumeric() - False
+# 3. не може містити великі літери - .isupper() - False
+# 4. не може містити пропуск і знаки пунктуації - string.punctuation - False
+# 5. "_" - можна
+# 6. не може бути жодним із зареєстрованих слів - keyword.kwlist - False
+a = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
-    break
- # and not value_int.isdigit() and not value_int.startswith(num) and not value_int in keyword.kwlist
-# 3
- # if digit.startswith(('1', '2','3')):
-    #     new_value_int = "False"
-    #     break
-
-
-
-
-
-
-
-
+# for x in some_val:
+#     if x is some_val.isupper():#цифри сприймає як велику літеру(
+#         print(False)
+#         break
+print(not some_val.startswith(a)
+      and not some_val.isnumeric()
+      and not keyword.iskeyword(some_val))
 
 
 
@@ -28,17 +28,10 @@ while True:
 
 
 
-    # if digit.isalpha():
-    #     new_value_int += upper
 
-# print(new_value_int)
-# value_int = "Ad"
-# if value_int.isupper():
-#     print('1 False')
-# elif value_int.isalnum():
-#     print("2 True")
-# elif value_int.isupper():
-#     print("3 True")
-# else:
-#     print('4 True')
-# print(value_int.isalnum())
+
+
+
+
+
+
