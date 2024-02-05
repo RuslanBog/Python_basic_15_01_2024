c82940ca96d8c648ff50b253a8d6069c049f5582
Ruslan <ruslanboginskiy@gmail.com>
2024-02-05 15:38:50 +0200

    home_work_5.1

---
+++ HW_5.1.1.py
@@ -0,0 +1,37 @@
+import string
+some_str = "6llK"
+name_val = ""
+
+# for name in some_str:
+#     if some_str.isnumeric():
+#         name_val = False
+#     else:
+#         name_val = True
+for name in some_str:
+    if some_str.isnumeric():
+        print(False)
+        # break
+    elif some_str.isupper():
+        print(False)
+        # break
+    # else:
+    #     print(True)
+        # break
+
+
+
+# for name in some_str:
+#     if some_str.isupper():
+#         name_val = False
+#         break
+# else:
+#     name_val = True
+# print(name_val)
+
+    # if name.isnumeric():
+    #     new_name_val = False
+    # if name.isupper():
+    #     new_name_val = False
+    # else:
+    #     new_name_val = True
+# print(new_name_val)
---
+++ HW_5.1.py
@@ -0,0 +1,44 @@
+import keyword
+
+value_int = "23lL"
+new_value_int = ""
+num = '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+while True:
+    print(not value_int.isupper())
+
+    break
+ # and not value_int.isdigit() and not value_int.startswith(num) and not value_int in keyword.kwlist
+# 3
+ # if digit.startswith(('1', '2','3')):
+    #     new_value_int = "False"
+    #     break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # if digit.isalpha():
+    #     new_value_int += upper
+
+# print(new_value_int)
+# value_int = "Ad"
+# if value_int.isupper():
+#     print('1 False')
+# elif value_int.isalnum():
+#     print("2 True")
+# elif value_int.isupper():
+#     print("3 True")
+# else:
+#     print('4 True')
+# print(value_int.isalnum())
