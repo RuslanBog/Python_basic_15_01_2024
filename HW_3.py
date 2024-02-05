b88756058ec0edf55b9ec36e4d89dfa8df8a70f0
Ruslan <ruslanboginskiy@gmail.com>
2024-02-05 15:38:25 +0200

    home_work_5.2

--- HW_3.py
+++ HW_3.py
@@ -1,18 +1,28 @@
-number_1 = float(input("Enter first number:"))
-number_2 = float(input("Enter second number:"))
-operator = input("Enter an operator (+, -, *, /):")
+running = True
+while running:
 
-if operator == "+":
-    print(number_1 + number_2)
-elif operator == "-":
-    print(number_1 - number_2)
-elif operator == "*":
-    print(number_1 * number_2)
-elif operator == "/":
-    if number_2 != 0:
-        print(number_1 / number_2)
+    number_1 = float(input("Enter first number: "))
+    number_2 = float(input("Enter second number: "))
+    operator = input("Enter an operator (+, -, *, /): ")
+
+    if operator == "+":
+        print(number_1 + number_2)
+    elif operator == "-":
+        print(number_1 - number_2)
+    elif operator == "*":
+        print(number_1 * number_2)
+    elif operator == "/":
+        if number_2 != 0:
+            print(number_1 / number_2)
+        else:
+            print("You can't divide by zero")
     else:
-        print("You can't divide by zero")
-else:
-    print("Try again")
+     print("Try again")
+
+    ask_question = input("Do you want to continue (y, n)?: ")
+    if ask_question == "y":
+        running = True
+    else:
+        running = False
+
 
