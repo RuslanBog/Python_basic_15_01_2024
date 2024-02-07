36f815bac0919de7dc3b1e9c0529c5887ef35e63
Ruslan <ruslanboginskiy@gmail.com>
2024-02-07 13:15:11 +0200

    home_work_5.1

--- HW_5.1.py
+++ HW_5.1.py
@@ -1,28 +1,44 @@
 
 import string
 import keyword
-# дано:
-some_val = "orjb2"
 
-# условие:
-# 1. не може починатися з цифри - startswith() - False
-# 2. не може складатися тільки з цифр - .isnumeric() - False
-# 3. не може містити великі літери - .isupper() - False
-# 4. не може містити пропуск і знаки пунктуації - string.punctuation - False
-# 5. "_" - можна
-# 6. не може бути жодним із зареєстрованих слів - keyword.kwlist - False
+# # умова:
+# # 1. не може починатися з цифри - startswith() - False
+# # 2. не може складатися тільки з цифр - .isnumeric() - False
+# # 3. не може містити великі літери - .isupper() - False
+# # 4. не може містити пропуск і знаки пунктуації - string.punctuation - False
+# # 5. "_" - можна
+# # 6. не може бути жодним із зареєстрованих слів - keyword.kwlist - False
+
+
+# some_str = '_'  # Вивід: True
+# some_str = 'x'  # Вивід: True
+# some_str = 'get_value'  # Вивід: True
+# some_str = 'get value'  # Вивід: False
+# some_str = 'get!value'  # Вивід: False
+# some_str = 'some_super_puper_value'  # Вивід: True
+# some_str = 'Get_value'  # Вивід: False
+# some_str = 'get_Value'  # Вивід: False
+# some_str = 'getValue'  # Вивід: False
+# some_str = '3m'  # Вивід: False
+some_str = 'm3'  # Вивід: True
+
+new_str = True
 a = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
-# for x in some_val:
-#     if x is some_val.isupper():#цифри сприймає як велику літеру(
-#         print(False)
-#         break
-print(not some_val.startswith(a)
-      and not some_val.isnumeric()
-      and not keyword.iskeyword(some_val))
+if some_str.startswith(a) or some_str in keyword.kwlist:
+    new_str = False
 
+for name in some_str:
+    if not new_str:
+        break
+    elif name == "_":
+        continue
+    elif name.isupper() or name in string.punctuation or name.isspace():
+        new_str = False
+        break
 
-
+print(new_str)
 
 
 
