1747848fb6bb1730dcd2218f9cefa18abdbf4127
Ruslan <ruslanboginskiy@gmail.com>
2024-02-14 13:59:04 +0200

    home_work_7.2

--- HW_7.2.py
+++ HW_7.2.py
@@ -1,37 +1,20 @@
 #
-# def correct_sentence(my_str):
-#     capt = my_str.capitalize()
-#     return capt
-# str = "ka"
-# my_str = str
-# print(correct_sentence(my_str))
-def correct_sentence(my_text):
-    my_text = my_text[0].upper() + my_text[1:]
-    return my_text
-    dot = my_str.find(".")
-    if dot == -1:
-        res = my_str + str_dot
-        return res
-    # else:
-    #     res = my_str + dot_str
-    #     return res
+def correct_sentence(text):
+
+    # text = text[0].upper() + text[1:]
+    if text[0].islower():
+        text = text[0].upper() + text[1:]
+
+    if text[-1] != '.':
+        text += "."
+
+    return text
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
 
 
-    # dot = my_str[:-1]
-    # if my_str[:-1] == ".":
-    #     return my_str
-    # else:
-    #     res = my_str + str_dot
-    #     return res
-
-text = "greetings, friends"
-my_text = text
-print(correct_sentence(my_text))
-
-
-# assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
-# assert correct_sentence("hello") == "Hello.", 'Test2'
-# assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
-# assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
-# assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
-# print('ОК')
