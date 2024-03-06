f44e06e88df7a32636c4ddd54033945bf1be8791
Ruslan <ruslanboginskiy@gmail.com>
2024-03-06 23:48:58 +0200

    home_work_14.3

---
+++ student.py
@@ -0,0 +1,11 @@
+# student.py
+from human import Human
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return super().__str__() + f', Record Book: {self.record_book}'
